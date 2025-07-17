c63faa1d02aaf63c9389415d17e3bf60b3a68b10
MatuqE <bordenavetuc@gmail.com>
2025-07-17 20:00:47 -0300

    agregado pagina pokemonApp

---
+++ PokemonApp.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getPokemons } from './src/store/slices/pokemon'
+
+export const PokemonApp = () => {
+
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+      dispatch( getPokemons() )
+
+    }, [])
+    
+  return (
+    <div>
+        <h1>PokemonApp</h1>
+        <hr />
+        <ul>
+            <li>Hola</li>
+            <li>Hola</li>
+            <li>Hola</li>
+        </ul>
+    </div>
+  )
+}
